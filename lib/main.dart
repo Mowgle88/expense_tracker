b0b117d821cc6a24c5bde60c2883de1d2b499a85
Dmitry Ilyin <mowgle>
2023-05-19 21:32:54 +0300

    feat: add setting and using a Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorSchema =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorSchema,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorSchema.onPrimaryContainer,
+            foregroundColor: kColorSchema.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSchema.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorSchema.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorSchema.onSecondaryContainer,
+                fontSize: 16)),
+      ),
       home: const Expenses(),
     ),
   );
