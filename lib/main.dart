6008218cea833b26e49069d8210308283b27b80b
Dmitry Ilyin <mowgle>
2023-05-20 10:10:37 +0300

    feat: add dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kColorSchema =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorSchema = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorSchema = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorSchema,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorSchema.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorSchema.primaryContainer,
+            foregroundColor: kDarkColorSchema.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData.dark().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kDarkColorSchema.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorSchema,
@@ -22,14 +52,18 @@ void main() {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-              backgroundColor: kColorSchema.primaryContainer),
+            backgroundColor: kColorSchema.primaryContainer,
+          ),
         ),
         textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
+              titleLarge: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: kColorSchema.onSecondaryContainer,
-                fontSize: 16)),
+                fontSize: 16,
+              ),
+            ),
       ),
+      // themeMode: ThemeMode.system, //default
       home: const Expenses(),
     ),
   );
